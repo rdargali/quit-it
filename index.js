b16dba6fc8de8ece9dda95a812c13c9d12e50942
Rawand Dargali <rdargali@yahoo.com>
2020-06-02 23:55:55 -0500

    mustache pages

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = requre("express");
+const express = require("express");
 const app = express();
 const session = require("express-session");
 
@@ -6,3 +6,16 @@ const mustacheExpress = require("mustache-express");
 app.use(express.static("public"));
 
 const PORT = process.env.PORT || 8080;
+
+// configure view engine
+app.engine("mustache", mustacheExpress());
+app.set("views", "./views");
+app.set("view engine", "mustache");
+
+app.get("/", (req, res) => {
+  res.render("landing");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
