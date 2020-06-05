5f19cd683efde5efee4a8ef7c4356bc080d2eb18
Rawand Dargali <rdargali@yahoo.com>
2020-06-04 22:56:57 -0500

    test

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ app.engine("mustache", mustacheExpress());
 app.set("views", "./views");
 app.set("view engine", "mustache");
 
-app.get("/", (req, res) => {
-  res.render("landing");
-});
+// app.get("/", (req, res) => {
+//   res.render("landing");
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
